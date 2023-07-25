db209762f43b9b2ed513f4fdcb5276bf9fc3dd9b
Lucas Farias <lucasf1@gmail.com>
2023-07-24 22:55:40 -0300

    Criação do projeto spring boot.

---
+++ src/test/java/io/github/lucasf1/StartupTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasf1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
