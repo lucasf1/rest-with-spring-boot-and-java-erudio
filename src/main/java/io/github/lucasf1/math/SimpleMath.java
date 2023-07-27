55f792b93edfd66649811d5480b510a7105393cc
Lucas Farias <lucasf1@gmail.com>
2023-07-26 22:45:42 -0300

    Criação do MathController e operações matemáticas.

---
+++ src/main/java/io/github/lucasf1/math/SimpleMath.java
@@ -0,0 +1,27 @@
+package io.github.lucasf1.math;
+
+public class SimpleMath {
+
+    public Double sum(Double num1, Double num2) {
+        return num1 + num2;
+    }
+
+    public Double subtraction(Double num1, Double num2) {
+        return num1 - num2;
+    }
+
+    public Double multiplication(Double num1, Double num2) {
+        return num1 * num2;
+    }
+
+    public Double division(Double num1, Double num2) {
+        return num1 / num2;
+    }
+
+    public Double mean(Double num1, Double num2) {
+        return (num1 + num2) / 2;
+    }
+    public Double squareRoot(Double num) {
+        return Math.sqrt(num);
+    }
+}
